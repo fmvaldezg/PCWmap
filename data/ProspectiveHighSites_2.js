7cdc9150864d7499fae42daddbbfd029ad81f110
fmvaldezg <fmvaldezg@yahoo.com>
2023-02-09 14:40:20 -0500

    changed files

---
+++ data/ProspectiveHighSites_2.js
@@ -0,0 +1,10 @@
+var json_ProspectiveHighSites_2 = {
+"type": "FeatureCollection",
+"name": "ProspectiveHighSites_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "qc_id": 0.0, "name": "Temple University 1", "description": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -75.1569417, 39.9811524 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 1.0, "name": "Temple University 2", "description": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -75.1523636, 40.0067504 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 2.0, "name": "167 W Allegheny Ave - HACE", "description": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -75.1317015, 39.9991374 ] ] } }
+]
+}
