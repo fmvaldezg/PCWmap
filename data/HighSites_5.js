38705f0624f05e1bed7d9925b69056659a9fbd88
fmvaldezg <fmvaldezg@yahoo.com>
2023-02-13 14:59:05 -0500

    changed index file and data

--- data/HighSites_5.js
+++
@@ -1,9 +0,0 @@
-var json_HighSites_5 = {
-"type": "FeatureCollection",
-"name": "HighSites_5",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "qc_id": 0.0, "name": "Gotham Tower", "description": "2000 N 2nd St" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -75.1373393, 39.9813486 ] ] } },
-{ "type": "Feature", "properties": { "qc_id": 1.0, "name": "Internet Exchange Point", "description": "401 N Broad St." }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -75.1617111, 39.9599187 ] ] } }
-]
-}
